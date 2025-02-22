7894c067261226261de568fb2116bee4b9ce2500
mrityunjay27 <mrityunjay27062000@gmail.com>
2025-02-22 23:00:48 +0530

    * Implemented catalogue view and service.

---
+++ bmes/cataloguebp/catalogue_service.py
@@ -0,0 +1,65 @@
+from bmes.cataloguebp.models import Brand, Category, StatusType, Product
+
+
+def fetch_products(request, category_slug, brand_slug):
+    """
+    Utility method to fetch product and does filtering if asked.
+    :param request:
+    :param category_slug:
+    :param brand_slug:
+    :return:
+    """
+    page = int(request.args.get('page', 1))
+
+    page_object = None
+
+    # Filter the products
+    if category_slug == 'all-categories' and brand_slug == 'all-brands':
+        page_object = Product.query.filter_by(product_status=StatusType.Active).paginate(page, 9, False)
+
+    if category_slug != 'all-categories' and brand_slug != 'all-brands':
+        page_object = (Product.query.filter_by(product_status=StatusType.Active)
+                       .filter(Product.categories.any(Category.slug == category_slug))
+                       .filter(Product.brands.any(Brand.slug == brand_slug)).paginate(page, 9, False)
+                       )
+    if category_slug != 'all-categories' and brand_slug == 'all-brands':
+        page_object = (Product.query.filter_by(product_status=StatusType.Active)
+                       .filter(Product.categories.any(Category.slug == category_slug)).paginate(page, 9, False)
+                       )
+    if category_slug == 'all-categories' and brand_slug != 'all-brands':
+        page_object = (Product.query.filter_by(product_status=StatusType.Active)
+                       .filter(Product.brands.any(Brand.slug == brand_slug)).paginate(page, 9, False)
+                       )
+
+    return page_object
+
+# The page_object has the following properties and methods:
+# items, pages, prev_num, next_num, iter_pages(), page, has_next, has_prev
+
+
+def fetch_products_V2(request, category_slug, brand_slug):
+    """
+    Utility method to fetch products and apply filtering if requested.
+    """
+    page = int(request.args.get('page', 1))
+
+    query = Product.query.filter_by(product_status=StatusType.Active)
+
+    if category_slug != 'all-categories':
+        query = query.filter(Product.categories.any(Category.slug == category_slug))
+
+    if brand_slug != 'all-brands':
+        query = query.filter(Product.brands.any(Brand.slug == brand_slug))
+
+    """
+    Query Execution
+    The query gets built dynamically based on the filters.
+    When .paginate(page, 9, False) is called, SQLAlchemy executes a LIMIT and OFFSET query behind the scenes.
+    
+    Pagination Mechanism
+    If page = 1, it retrieves products 1-9 (LIMIT 9 OFFSET 0).
+    If page = 2, it retrieves products 10-18 (LIMIT 9 OFFSET 9).
+    If page = 3, it retrieves products 19-27 (LIMIT 9 OFFSET 18).
+    """
+
+    return query.paginate(page, 9, False)
--- bmes/cataloguebp/views.py
+++ bmes/cataloguebp/views.py
@@ -1,3 +1,41 @@
 from flask import  Blueprint, request, session, render_template, url_for, redirect
+from datetime import datetime
+from bmes.cataloguebp import catalogue_service
 
 catalogue = Blueprint('catalogue', __name__, template_folder='templates/cataloguebp')
+
+@catalogue.route('/catalogue', methods=['GET','POST'])
+@catalogue.route('/catalogue/<category_slug>/<brand_slug>/', methods=['GET','POST'])
+def catalogue_view(category_slug='all-categories', brand_slug='all-brands'):
+
+     if request.method == 'POST': # Adding item to the cart
+
+         #Fetching products from the catalogue service
+         page_object = catalogue_service.fetch_products(request, category_slug, brand_slug)
+
+         return render_template(
+           'catalogue.html',
+           title='Product Page',
+           year=datetime.now().year,
+           page_object= page_object,
+           selected_category=category_slug,
+           selected_brand=brand_slug,
+         )
+     else: # Get list of all the product.
+         page_object = catalogue_service.fetch_products(request, category_slug, brand_slug)
+
+         return render_template(
+           'catalogue.html',
+           title='Product Page',
+           year=datetime.now().year,
+           page_object= page_object,
+           selected_category=category_slug,
+           selected_brand=brand_slug,
+         )
+
+@catalogue.route('/catalogue/products/<product_slug>/')
+def product_detail_view(product_slug):
+    return render_template(
+           title='Product Page',
+           year=datetime.now().year,
+         )
