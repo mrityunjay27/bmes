7495507d92b29523e66443c51763d7a946b49670
mrityunjay27 <mrityunjay27062000@gmail.com>
2025-03-01 14:20:24 +0530

    * HTML work for catalog bp

--- bmes/cataloguebp/__init__.py
+++ bmes/cataloguebp/__init__.py
@@ -1,1 +1,15 @@
 from bmes.cataloguebp import views, models, catalogue_service
+
+@views.catalogue.context_processor
+def inject_context():
+    """
+    This data will be exposed within this context. All templates, all files will have access,
+    basically in catalogue blueprint
+    :return:
+    """
+    return {
+              'categories': models.Category.query.filter_by(category_status= models.StatusType.Active).all(),
+              'brands': models.Brand.query.filter_by(brand_status=models.StatusType.Active).all(),
+              'selected_category':'all-categories',
+              'selected_brand':'all-brands',
+           }
